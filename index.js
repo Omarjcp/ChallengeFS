b962714d2ceb781b60fe0a5a492e3820230656e3
Omarjcp <omar.castillo1095@gmail.com>
2021-08-17 00:19:03 -0300

    done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ alter: false }).then(() => {
-  server.listen(3001, async () => {
+const port = process.env.PORT || 3001;
+
+conn.sync({ force: false }).then(() => {
+  server.listen(port, async () => {
     console.log("servidor arrancado! en el puerto 3001");
   });
 });
