d77708d93bd092e2cfae221edd8ac74d5b6ba0ca
Omarjcp <omar.castillo1095@gmail.com>
2021-11-09 21:18:28 -0300

    fix get products for categories

--- src/controllers/productos/obtenerProductos.contr.js
+++ src/controllers/productos/obtenerProductos.contr.js
@@ -1,11 +1,10 @@
-const { Producto, Categoria } = require("../../db");
+const { Producto, Categoria, Relleno, Sabor } = require("../../db");
 const { obtenerProductosDb } = require("../funciones/obtenerDb");
 const { filtradoNombre } = require("../funciones/filtrado.js");
 
 const obtenerProductos = async (req, res) => {
   try {
-    let { name, categoria, relleno, sabor, categoryId, rellenoId, saborId } =
-      req.query;
+    let { name, categoryId, rellenoId, saborId } = req.query;
 
     let productosDb = await obtenerProductosDb();
 
@@ -23,50 +22,23 @@ const obtenerProductos = async (req, res) => {
         });
       }
     }
-    if (categoryId) {
-      let productosDb = await Producto.findAll({
-        where: {
-          categoriumId: categoryId ? categoryId : null,
-        },
+    if (categoryId || rellenoId || saborId) {
+      let productosDb = await Producto.findAll();
+
+      let productsFilter = productosDb.filter((product) => {
+        if (
+          Number(product.categoriumId) === Number(categoryId) ||
+          Number(product.saborId) === Number(saborId) ||
+          Number(product.rellenoId) === Number(rellenoId)
+        ) {
+          return product;
+        }
       });
-      if (productosDb.length > 0) {
+
+      if (productsFilter.length > 0) {
         res.json({
           tipo: "producto segun id de categoria pasado por param",
-          data: productosDb,
-        });
-      } else {
-        res.json({
-          msg: "producto no encontrado",
-        });
-      }
-    }
-    if (rellenoId) {
-      let productosDb = await Producto.findAll({
-        where: {
-          rellenoId: rellenoId ? rellenoId : null,
-        },
-      });
-      if (productosDb.length > 0) {
-        res.json({
-          tipo: "producto segun id de categoria pasado por param",
-          data: productosDb,
-        });
-      } else {
-        res.json({
-          msg: "producto no encontrado",
-        });
-      }
-    }
-    if (saborId) {
-      let productosDb = await Producto.findAll({
-        where: {
-          saborId: saborId ? saborId : null,
-        },
-      });
-      if (productosDb.length > 0) {
-        res.json({
-          tipo: "producto segun id de categoria pasado por param",
-          data: productosDb,
+          data: productsFilter,
         });
       } else {
         res.json({
